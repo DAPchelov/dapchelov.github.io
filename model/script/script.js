a648e53be6621668008cf5726fd4c99eeb553437
DAPchelov <96956609+DAPchelov@users.noreply.github.com>
2022-02-08 22:15:22 +0400

    Add files via upload

--- model/script/script.js
+++ model/script/script.js
@@ -1,45 +1,72 @@
-let i = 0; // хранит текущее состояние слайдера
-const sliderLine = document.getElementsByClassName("slider-line")[0]; // прокручиваемая полоса (её будем смещать)
-const tabs = document.getElementsByClassName("tabItem"); // кнопки в нижней части слайдера
-const colorBtnUnPress = "rgb(226, 255, 253)" //цвет отжатой кнопки
-const colorBtnPress = "rgb(184, 213, 211)" //цвет нажатой кнопки
+let currentSlideIndex = 0;
+const sliderLineEl = document.getElementsByClassName("slider-line")[0]; // прокручиваемая полоса (её будем смещать)
+const tabsEls = document.getElementsByClassName("tabItem"); // кнопки в нижней части слайдера
+const colorBtnUnPress = "rgb(226, 255, 253)";
+const colorBtnOnpress = "rgb(184, 213, 211)";
 
-tabs[0].style.backgroundColor = colorBtnPress; // первая кнопка нажата в начальном состоянии слайдера
+/** ---------------------------------- */
+function makeFirstTabOnpressed() {
+    tabsEls[0].style.backgroundColor = colorBtnOnpress;
+}
 
-// слушает кнопкки, по нажатию вызывает функцию скролла
-document.getElementById("nextBtn").addEventListener("click", nextSlide);
-document.getElementById("prevBtn").addEventListener("click", prevSlide);
-
-/* функции скролла. Отжимает текущую нижнюю кнопку, увеличивает i, делает нажатой следующую нижнюю кнопку
-и смещает слайдер влево на 1000px, при увеличении i не даёт значению выйти за диапозон больше количества кнопок */
-function nextSlide () {
-    tabs[i].style.backgroundColor = colorBtnUnPress;
-    i++;
-    if (i > tabs.length - 1) {
-        i = 0;
+function registerListeners() {
+    document.getElementById("nextBtn").addEventListener("click", scrollToNextSlide);
+    document.getElementById("prevBtn").addEventListener("click", scrollToPrevSlide);
+    for (let j = 0; j < tabsEls.length; j++) {
+        tabsEls[j].addEventListener("click", function () {
+            sliderLineEl.style.left = -j * 1000 + "px";
+            makeCurrentTabUnpressed();
+            currentSlideIndex = j;
+            makeCurrentTabOnpressed();
+        });
     }
-    tabs[i].style.backgroundColor = colorBtnPress;
-    sliderLine.style.left = -i * 1000 + "px";
 }
 
-// тоже самое, но i уменьшается, слайдер крутится в обратную сторону
-function prevSlide () {
-    tabs[i].style.backgroundColor = colorBtnUnPress;
-    i--;
-    if (i < 0) {
-        i = tabs.length - 1;
+function makeCurrentTabUnpressed() {
+    tabsEls[currentSlideIndex].style.backgroundColor = colorBtnUnPress;
+}
+
+function makeCurrentTabOnpressed() {
+    tabsEls[currentSlideIndex].style.backgroundColor = colorBtnOnpress;
+}
+
+function moveCurrentSlideToVisibleArea() {
+    sliderLineEl.style.left = -currentSlideIndex * 1000 + "px";
+}
+
+function validateCurrentSlideIndex(isIncrement) {
+    if (isIncrement) {
+        if (currentSlideIndex > tabsEls.length - 1) {
+            currentSlideIndex = 0;
+        }
+    } else {
+        if (currentSlideIndex < 0) {
+            currentSlideIndex = tabsEls.length - 1;
+        }
     }
-    tabs[i].style.backgroundColor = colorBtnPress;
-    sliderLine.style.left = -i * 1000 + "px";
 }
 
-/* всем кнопкам внизу слайдера назначается слушатель клика. По клику слайдер сдвигается соответсвенно номеру нажатой кнопки,
-* текущая нажатая кнопка отжимается, текущему номеру присваивается новое значение, кнопка с новым номером нажимается*/
-for (let j = 0; j < tabs.length; j++) {
-    tabs[j].addEventListener("click", function () {
-        sliderLine.style.left = -j * 1000 + "px";
-        tabs[i].style.backgroundColor = colorBtnUnPress;
-        i = j;
-        tabs[i].style.backgroundColor = colorBtnPress;
-    });
+function scrollToNextSlide () {
+    makeCurrentTabUnpressed();
+
+    currentSlideIndex++;
+    validateCurrentSlideIndex(true);
+
+    makeCurrentTabOnpressed();
+    moveCurrentSlideToVisibleArea();
 }
+
+function scrollToPrevSlide () {
+    makeCurrentTabUnpressed();
+
+    currentSlideIndex--;
+    validateCurrentSlideIndex(false);
+
+    makeCurrentTabOnpressed();
+    moveCurrentSlideToVisibleArea();
+}
+
+/** ---------------------------------- */
+
+makeFirstTabOnpressed();
+registerListeners();

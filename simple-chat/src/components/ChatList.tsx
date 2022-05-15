1844ab0826f6b74d3993c56e4ef3e3f27a21545e
DAPchelov <96956609+DAPchelov@users.noreply.github.com>
2022-05-16 01:01:33 +0400

    all messages fetch from server and render in frame

--- simple-chat/src/components/ChatList.tsx
+++ simple-chat/src/components/ChatList.tsx
@@ -1,32 +1,69 @@
-
-import List from '@mui/material/List';
-import React from 'react';
-import { ChatListItem } from './ChatListItem';
-
+import List from "@mui/material/List";
+import React, { useEffect, useState } from "react";
+import { ChatListItem } from "./ChatListItem";
 
 const ChatList = () => {
-    
-    const makeChatList = (props: number) => {
-        let list = [];
-        for (let i = 0; i < props; i++) {
-            list.push(<ChatListItem name='testName' message='testMessge'/>);
-        }
-        return (list);
+  interface Imessage {
+    id: number;
+    name: string;
+    text: string;
+  }
+
+  const MakeChatList = (props: number) => {
+    const [messagesList, setMessagesList] = useState<Imessage[]>([]);
+
+    useEffect(() => {
+      const response = fetch("http://localhost:4000/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          query: `{
+                            messages {
+                              id
+                              name
+                              text
+                            }
+                          }`
+        })
+      })
+        .then(response => response.json())
+        .then(json => setMessagesList(json.data.messages));
+    }, []);
+    let list = [];
+    if (messagesList == undefined) {
+      list.push(<ChatListItem name="Loading" message="Loading" />);
+      return list;
+    } else {
+      for (let i = 0; i < messagesList.length; i++) {
+        list.push(
+          <ChatListItem
+            key={messagesList[i].id}
+            name={messagesList[i].name}
+            message={messagesList[i].text}
+          />
+        );
+      }
     }
 
-    return (
-        <div className='body-Frame-ChatList'>
-            <List
-                sx={{
-                    width: '100%',
-                    maxWidth: 360,
-                    bgcolor: 'background.paper',
-                }}
-            >
-                {makeChatList(4)}
-            </List>
-        </div>
-    )
-}
+    return list;
+  };
 
-export { ChatList }
+  return (
+    <div className="body-Frame-ChatList">
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper"
+        }}
+      >
+        {MakeChatList(4)}
+      </List>
+    </div>
+  );
+};
+
+export { ChatList };

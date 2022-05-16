4a363de9e51c1a24889280db90554a60094d18ae
DAPchelov <96956609+DAPchelov@users.noreply.github.com>
2022-05-17 01:56:37 +0400

    make sending and receiving messages

--- graphql-server/src/db.js
+++ graphql-server/src/db.js
@@ -1,4 +1,5 @@
 export let messages = [
-  { id: 1, name: 'John Doe', text: 'TextText' },
-  { id: 2, name: 'Leeroy Jenkins', text: 'someText' },
+  { id: '1', name: 'John Doe', text: 'TextText' },
+  { id: '2', name: 'Leeroy Jenkins', text: 'someText' },
+  { id: '3', name: 'Lyra Heartstrings', text: 'someTextLyra' },
 ];
--- graphql-server/src/resolvers.js
+++ graphql-server/src/resolvers.js
@@ -3,39 +3,31 @@ import { messages } from './db';
 const resolvers = {
   Query: {
     message: (parent, { id }, context, info) => {
-      return messages.find(user => user.id == id);
+      return messages.find(message => message.id == id);
     },
     messages: (parent, args, context, info) => {
       return messages;
     },
   },
-  // Mutation: {
-  //   createUser: (parent, { id, name, email, age }, context, info) => {
-  //     const newUser = { id, name, email, age };
 
-  //     users.push(newUser);
+  Mutation: {
+    createMessage: (parent, { id, name, text }, context, info) => {
+      const newMessage = { id, name, text };
 
-  //     return newUser;
-  //   },
-  //   updateUser: (parent, { id, name, email, age }, context, info) => {
-  //     let newUser = users.find(user => user.id === id);
+      messages.push(newMessage);
 
-  //     newUser.name = name;
-  //     newUser.email = email;
-  //     newUser.age = age;
+      return newMessage;
+    },
+    deleteMessage: (parent, { id }, context, info) => {
+      const messageIndex = messages.findIndex(message => message.id == id);
 
-  //     return newUser;
-  //   },
-  //   deleteUser: (parent, { id }, context, info) => {
-  //     const userIndex = users.findIndex(user => user.id === id);
+      if (messageIndex === -1) throw new Error('Message not found.');
 
-  //     if (userIndex === -1) throw new Error("User not found.");
+      const deletedMessages = users.splice(messageIndex, 1);
 
-  //     const deletedUsers = users.splice(userIndex, 1);
-
-  //     return deletedUsers[0];
-  //   }
-  // }
+      return deletedUsers[0];
+    },
+  },
 };
 
 export default resolvers;

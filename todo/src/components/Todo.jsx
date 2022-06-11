30dc9e8763d29be5ccd1f3ee869acdebd0da0867
DAPchelov <96956609+DAPchelov@users.noreply.github.com>
2022-06-11 16:43:35 +0400

    refactor Todo compnent again

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,29 +1,24 @@
 import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 
 const Todo = ({ task, handleToggle }) => {
     return (
-    <div>
-        <Paper elevation={1}>
-        <ListItem disablePadding aria-multiline>
-            <Checkbox
-                checked={task.complete}
-                onClick={() => handleToggle(task.id)}
-            />
-            <ListItemText 
-            primaryTypographyProps={{ 
-                style: {
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis'
-                }
-            }}
-            primary={task.content} />
-        </ListItem>
-        </Paper>
-    </div>
+        <div>
+            <Paper elevation={1}>
+                <ListItem disablePadding aria-multiline>
+                    <Checkbox
+                        checked={task.complete}
+                        onClick={() => handleToggle(task.id)}
+                    />
+                    <Typography sx={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                        {task.content}
+                    </Typography>
+                </ListItem>
+            </Paper>
+        </div>
     );
 }
 

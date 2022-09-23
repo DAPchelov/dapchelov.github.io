2056080f5181bc61f63c3598c06bbd786030e7da
DAPchelov <pcheld@proton.me>
2022-09-23 13:36:09 +0400

    test commit

--- advanced-todo/client/src/components/Main.tsx
+++ advanced-todo/client/src/components/Main.tsx
@@ -9,7 +9,7 @@ import Typography from '@mui/material/Typography';
 import TodoList from './TodoList'
 
 
-
+const deleteMe: string = 'deleteMe';
 
 const Main:React.FC = () => {
   const [completed, setCompleted] = useState<boolean | undefined>(undefined);

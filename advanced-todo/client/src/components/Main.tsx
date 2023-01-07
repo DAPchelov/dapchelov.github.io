0a1482792c5fd54cdf2124843446efd6169a09a6
DAPchelov <pcheld@proton.me>
2023-01-07 23:58:26 +0400

    Refactoring structure and appearance

--- advanced-todo/client/src/components/Main.tsx
+++ advanced-todo/client/src/components/Main.tsx
@@ -1,19 +1,12 @@
-import { useContext } from "react";
-import InputForm from "./InputForm";
-import SelectButtons from "./SelectButtons";
-import ClearButton from "./ClearButton";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import { Container } from "@mui/system";
 import Typography from '@mui/material/Typography';
-import TodoList from './TodoList'
-import { Context } from '../../src/index'
-import { observer } from "mobx-react-lite";
+import Box from "@mui/material/Box";
 
+import TodoList from './TodoList'
+import InputForm from "./InputForm";
+import ControlPanel from "./ControlPanel";
 
 const Main: React.FC = () => {
-
-  const store = useContext(Context);
     
   return (
     <Container maxWidth="sm" sx={{
@@ -27,24 +20,11 @@ const Main: React.FC = () => {
       </Typography>
       <Box sx={{ width: "100%", maxWidth: 600 }}>
         <InputForm />
+        <ControlPanel />
         <TodoList />
-        <Paper
-          elevation={1}
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center"
-          }}
-        >
-          <Typography color="text.secondary" ml={1} sx={{ fontSize: 14 }}>
-            {`${store.todos.filter(task => task.isCompleted === false).length}`.concat(" items left")}
-          </Typography>
-          <SelectButtons />
-          <ClearButton />
-        </Paper>
       </Box>
     </Container>
   );
 };
 
-export default observer(Main);
+export default Main;
--- advanced-todo/client/src/components/Todo.tsx
+++ advanced-todo/client/src/components/Todo.tsx
@@ -1,29 +1,41 @@
 import ListItem from '@mui/material/ListItem';
 import Checkbox from '@mui/material/Checkbox';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import React, { useContext } from 'react';
 import { Context } from '../../src/index'
 import { ITodo } from "../models/ITodo";
+import { height, width } from '@mui/system';
 
 const Todo: React.FC<ITodo> = (props: ITodo) => {
     const store = useContext(Context);
 
     return (
         <Paper elevation={1} sx={{
-            cursor: 'pointer',
             '&:hover': {
-                background: 'linear-gradient(90deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 83%, rgba(21,101,192,0.15) 100%);'
+                background: 'linear-gradient(270deg, rgba(255,85,0,0.15) 0%, rgba(0,0,0,0) 17%, rgba(0,0,0,0) 83%, rgba(21,101,192,0.15) 100%)'
             }
         }}
-        onClick={()=> store.checkTodo(props._id, !props.isCompleted)}>
+        >
             <ListItem disablePadding aria-multiline>
                 <Checkbox
                     checked={props.isCompleted}
-                />
-                <Typography sx={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                    onClick={()=> store.checkTodo(props._id, !props.isCompleted)}
+                    sx={{
+                        cursor: 'pointer',
+                    }}/>
+                <Typography sx={{ overflow: 'hidden', textOverflow: 'ellipsis', width: '88%'}}>
                     {props.message}
                 </Typography>
+                <DeleteIcon fontSize="small"
+                sx={{
+                    cursor: 'pointer',
+                    position: 'absolute',
+                    right: '5px',
+                }}/>
             </ListItem>
         </Paper>
     );

abe94748203de9939c8c78caac1627a16d47a84a
DAPchelov <pcheld@proton.me>
2024-04-02 10:51:07 +0000

    Fix double user entry error

--- wscards/server/models/group-model.js
+++ wscards/server/models/group-model.js
@@ -1,18 +1,17 @@
 import mongoose from 'mongoose';
-import { CardSchema } from './cardsList-model.js';
 
 const Schema = mongoose.Schema;
 const model = mongoose.model;
 
 const GroupUserSchema = new Schema({
-    userId: { type: String, unique: false, required: false },
-    isLoggedIn: { type: Boolean, required: false, default: false },
+    userId: { type: String, unique: false, required: true },
+    isLoggedIn: { type: Boolean, required: true, default: false },
 })
 
 const GroupSchema = new Schema({
     label: { type: String, unique: true, required: true },
     ownerId: { type: String, required: true },
-    users:[GroupUserSchema]
+    users:[GroupUserSchema],
 })
 
 const GroupeModel = model('Group', GroupSchema);
